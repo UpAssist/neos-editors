a438f10b93cb0387c5e5dc77589725da44e5cb70
Henjo Hoeksma <henjo@upassist.com>
2024-11-13 11:24:58 +0100

    updated reference

--- Resources/Private/Scripts/src/manifest.js
+++ Resources/Private/Scripts/src/manifest.js
@@ -1,9 +1,9 @@
 import manifest from "@neos-project/neos-ui-extensibility";
 import ArrayInfoEditor from "./ArrayInfoEditor";
 
-manifest('UpAssist.ArrayInfoEditor', {}, (globalRegistry) => {
+manifest('UpAssist.Neos.Editors.ArrayInfoEditor', {}, (globalRegistry) => {
 	const editorsRegistry = globalRegistry.get('inspector').get('editors');
-	editorsRegistry.set("UpAssist/ArrayInfoEditor", {
+	editorsRegistry.set("UpAssist.Neos.Editors/Inspector/Editors/ArrayInfoEditor", {
 		component: ArrayInfoEditor,
 	});
 });
